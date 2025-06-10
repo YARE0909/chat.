2d6410359f95f9f6ff3b43da3ee03b22b5c0f756
YARE0909 <pradyumnad092004@gmail.com>
2025-06-10 15:24:58 +0530

    Minor UI change

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -95,7 +95,7 @@ export function MessageInput({ onSend }: MessageInputProps) {
             {files.map((f, i) => (
               <div
                 key={i}
-                className="flex items-center space-x-1 bg-white text-black px-3 py-2 rounded-full text-xs relative"
+                className="flex items-center space-x-1 bg-white/10 text-white px-3 py-2 rounded-full text-xs relative"
               >
                 {/\.(jpe?g|png|gif)$/i.test(f.name) ? (
                   <ImageIcon size={16} />
